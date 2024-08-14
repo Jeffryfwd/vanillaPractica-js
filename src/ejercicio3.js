c6d9ffc13d3505148bf9ff3a728156819cd9d249
Jeffryfwd <jmedina@fwdcostarica.com>
2024-08-14 15:05:03 -0600

    3 finalizado

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,24 @@
-// Inserte el código aquí
+function clonarArreglo(arreglo) {
+    return [...arreglo];
+}
+const arreglo = [1,2,3,4];
+const arregloClonado = clonarArreglo(arreglo);
+console.log(arregloClonado);
+//-----------------------------------------------
+
+function clonarObjeto(objeto) {
+    return{...objeto}
+    
+}
+const objeto={
+    nombre: 'jeff',
+    apellido: 'medina'
+};
+const objetoClonado=clonarObjeto(objeto)
+console.log(objetoClonado);
+
 
 
 
 // NO MODIFICAR
-export { clonarArreglo, clonarObjeto, sumar, saludar };
+//export { clonarArreglo, clonarObjeto, sumar, saludar };
